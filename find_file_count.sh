9e912e444b21fac76d0af29738bea71825775fbb
dhruv-kumar-iopshub <dhruv.kumar@iopshub.com>
2023-02-07 13:12:43 +0530

    Symbolic count

--- find_file_count.sh
+++ find_file_count.sh
@@ -1,48 +1,20 @@
 #!/bin/bash
 
-LOCATION=$1
 FILECOUNT=$num_files
 DIRCOUNT=$num_directories
 SYMBOLIC=$num_symlinks
-if [ "$#"! == "1" ]
-then
-    echo You have to give only one parameter.
-exit 1
-fi
-while (( $# ))
-    do
-        echo You gave me $1
-shift 
+while getopts ":p:" option;
+do
+case $option in
+p)
+echo received -p
+LOCATION=$p
+;;
+esac
 done
 num_files=$(find $LOCATION -type f | wc -l)
 num_directories=$(find $LOCATION -type d | wc -l)
 num_symlinks=$(find $LOCATION -type l | wc -l)
-#for d in $num_files
-#do
- #  DIRCOUNT=$[$DIRCOUNT+1]
-#done
-
-#for f in $FILES
-#do
- #  FILECOUNT=$[$FILECOUNT+1]
-#done
-#for l in $FILES
-#do 
- #  SYMBOLIC=$[SYMBOLIC+1]
-#done
-#for item in $LOCATION
-#do
-#if [ -f "$item" ]
- #   then
-  #       FILECOUNT=$[$FILECOUNT+1]
-   # elif [ -d "$item" ]
-    #    then
-     #    DIRCOUNT=$[$DIRCOUNT+1]
-   # elif [ -l "$item" ]
-   #     then 
-#	 SYMBOLIC=$[$SYMBOLIC+1]   
-#fi
-#done
 
 echo "File count: " $num_files
 echo "Directory count: " $num_directories
