8bc1d6dfb6f9b5e88506083241433cca0df94bc1
dhruv-kumar-iopshub <dhruv.kumar@iopshub.com>
2023-02-07 13:01:21 +0530

    File Count

--- find_file_count.sh
+++ find_file_count.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 LOCATION=$1
-FILECOUNT=0
-DIRCOUNT=0
-
-if [ "$#" == "1" ]
+FILECOUNT=$num_files
+DIRCOUNT=$num_directories
+SYMBOLIC=$num_symlinks
+if [ "$#"! == "1" ]
 then
-    echo You have to give at least one parameter.
+    echo You have to give only one parameter.
 exit 1
 fi
 while (( $# ))
@@ -14,30 +14,36 @@ while (( $# ))
         echo You gave me $1
 shift 
 done
+num_files=$(find $LOCATION -type f | wc -l)
+num_directories=$(find $LOCATION -type d | wc -l)
+num_symlinks=$(find $LOCATION -type l | wc -l)
+#for d in $num_files
+#do
+ #  DIRCOUNT=$[$DIRCOUNT+1]
+#done
 
-DIRS=$(find $LOCATION -type d)
-FILES=$(find $LOCATION -type f)
+#for f in $FILES
+#do
+ #  FILECOUNT=$[$FILECOUNT+1]
+#done
+#for l in $FILES
+#do 
+ #  SYMBOLIC=$[SYMBOLIC+1]
+#done
+#for item in $LOCATION
+#do
+#if [ -f "$item" ]
+ #   then
+  #       FILECOUNT=$[$FILECOUNT+1]
+   # elif [ -d "$item" ]
+    #    then
+     #    DIRCOUNT=$[$DIRCOUNT+1]
+   # elif [ -l "$item" ]
+   #     then 
+#	 SYMBOLIC=$[$SYMBOLIC+1]   
+#fi
+#done
 
-for d in $DIRS
-do
-   DIRCOUNT=$[$DIRCOUNT+1]
-done
-
-for f in $FILES
-do
-   FILECOUNT=$[$FILECOUNT+1]
-done
-
-for item in $LOCATION
-do
-if [ -f "$item" ]
-    then
-         FILECOUNT=$[$FILECOUNT+1]
-    elif [ -d "$item" ]
-        then
-         DIRCOUNT=$[$DIRCOUNT+1]
-fi
-done
-
-echo "File count: " $FILECOUNT
-echo "Directory count: " $DIRCOUNT
+echo "File count: " $num_files
+echo "Directory count: " $num_directories
+echo "Symbloic count: " $num_symlinks
